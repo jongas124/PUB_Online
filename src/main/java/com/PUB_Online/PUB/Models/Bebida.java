d8494abb8ce9844df4335d2a217656e74fd8e801
Mauro Sérgio <131288294+Omega050@users.noreply.github.com>
2024-07-26 20:44:00 -0300

    Starting models

---
+++ src/main/java/com/PUB_Online/PUB/Models/Bebida.java
@@ -0,0 +1,29 @@
+package com.PUB_Online.PUB.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+public class Bebida {
+    private String tamanho;
+    private int estoque;
+
+    public int getEstoque() {
+        return estoque;
+    }
+    public String getTamanho() {
+        return tamanho;
+    }
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+}
---
+++ src/main/java/com/PUB_Online/PUB/Models/ItemMenu.java
@@ -0,0 +1,44 @@
+package com.PUB_Online.PUB.Models;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class ItemMenu {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Double preco;
+
+    public ItemMenu() {}
+
+    public ItemMenu(String nome, Double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+}
