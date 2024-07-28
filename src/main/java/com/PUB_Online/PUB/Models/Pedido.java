d19ceb3b6866e3fc7579664b0ff54bd3e736b37e
Mauro Sérgio <131288294+Omega050@users.noreply.github.com>
2024-07-27 22:07:39 -0300

    Updating Pedido

--- src/main/java/com/PUB_Online/PUB/Models/Pedido.java
+++ src/main/java/com/PUB_Online/PUB/Models/Pedido.java
@@ -3,12 +3,15 @@ package com.PUB_Online.PUB.Models;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,10 +28,34 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
-
-    @Column(name = "data")
+    
+    @CreationTimestamp //Conferir Depois
+    @Column(name = "data", updatable = false)
     private LocalDate data;
 
-    @Column(name = "hora")
+    @CreationTimestamp //Conferir Depois
+    @Column(name = "hora", updatable = false)
     private LocalTime hora;
+
+    @Column(name = "preco")
+    @NotNull
+    private float preco;
+
+    @Column(name = "comanda") //Era pra preparar o link pra comanda, mas não acho q carregar ela seja uma boa
+    @NotNull
+    private int numeroComanda;
+
+    @Column(name = "status", nullable = false)
+    @NotNull
+    private Status status;
+    
+    @Getter
+    @AllArgsConstructor
+    public enum Status {
+        FILA(1),
+        PREPARANDO(2),
+        ENTREGUE(3);
+
+        private int code;
+    }
 }
