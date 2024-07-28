efde3916303a01cb152654cc09c87c6101169a0e
jongas124 <kossos124@gmail.com>
2024-07-28 03:38:52 -0300

    Adequação nome da pasta "models"
    Mudança de nome da pasta "models" e, consequentemente, dos packages para adequação

--- src/main/java/com/PUB_Online/PUB/Models/ItemMenu.java
+++ src/main/java/com/PUB_Online/PUB/Models/ItemMenu.java
@@ -1,4 +1,4 @@
-package com.PUB_Online.PUB.Models;
+package com.PUB_Online.PUB.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
--- src/main/java/com/PUB_Online/PUB/Models/Pedido.java
+++ src/main/java/com/PUB_Online/PUB/Models/Pedido.java
@@ -1,4 +1,4 @@
-package com.PUB_Online.PUB.Models;
+package com.PUB_Online.PUB.models;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
